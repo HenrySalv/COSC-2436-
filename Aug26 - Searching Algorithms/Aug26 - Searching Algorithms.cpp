10fccc6af73808dee9aa83e1487119ae6baf60f4
a00434992 <a00434992@B7QHF24>
2025-08-26 15:40:46 -0500

    Add project files.

---
+++ Aug26 - Searching Algorithms/Aug26 - Searching Algorithms.cpp
@@ -0,0 +1,28 @@
+// Aug26 - Searching Algorithms.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <vector>
+#include <iostream>
+#include <string>
+//#include"mySpecialHeaderFile.h"
+
+int searchMyRoom(const std::vector<std::string>& stuffInMyRoom) 
+{
+
+}
+
+int main()
+{
+    std::vector < std::string > stuffInMyRoom =
+    {
+        "socks",
+        "keys",
+        "lamp",
+        "chair",
+        "laptop",
+        "dusty bunny",
+        "mug",
+    };
+}
+
+
+
